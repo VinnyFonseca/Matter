6b0e8a19ed1b40aae232e71259aabd3500cdfe88
Vinny Fonseca <vinnyfonseca11@gmail.com>
2015-01-24 23:41:28 +0000

    GA Tracking code

--- include/foot.php
+++ include/foot.php
@@ -12,14 +12,16 @@
 		-->
 
 		<!-- Google Analytics -->
+		<!--
 		<script type="text/javascript">
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 			})(window,document,'script','//www.google-analytics.com/analytics.js','_gaTracker');
 
-			_gaTracker('create', 'UA-26718745-2', 'auto');
+			_gaTracker('create', 'UA-XXXXXXXX-X', 'auto');
 			_gaTracker('send', 'pageview');
 		</script>
+		-->
 	</body>
 </html>
