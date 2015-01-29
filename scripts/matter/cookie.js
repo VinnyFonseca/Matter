301063d16d44b538f177e7d05959b59f06ba7248
Vinny Fonseca <vinny@vinny-VirtualBox.(none)>
2015-01-29 16:09:54 +0000

    Linux experimentation

--- scripts/matter/cookie.js
+++ scripts/matter/cookie.js
@@ -44,12 +44,11 @@ function initCookies() {
 
 	if ( firstVisitCookie === null ) {
 		cookieSystem.set("firstVisit", "yes", 365);
-		var cookieType = "bar",
-			cookieDelay = 0,
+		var cookieDelay = 0,
 			cookieTone = "warning",
 			cookieMessage = "This is a sample text stating how Matter uses cookies to give you a better experience. View our <a href='#' target='_blank'>Privacy Policy</a> to read more about how we use them. By continuing to browse you are accepting our <a href='#' target='_blank'>Terms &amp; Conditions</a>.";
 
-		notify(cookieMessage, cookieTone, cookieType, cookieDelay);
+		notify(cookieMessage, cookieTone, cookieDelay);
 	}
 
 	if ( config.application.debug ) console.log("Init :: Cookie System");
